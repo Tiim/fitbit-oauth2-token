c07971891b7cf9b421e025cf6ef0db14f373ead4
Tiim <tim.bachmann96@gmail.com>
2022-04-11 21:27:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fitbit Auth Token
+
+## How to use
+
+1. Clone this repo
+2. Serve the folder as http: `npx serve`
+3. In a separate terminal: run a https tunneling service: `npx localtunnel --port 3000`
+4. Open the webpage
+5. Update the redirect url in your [dev.fitbit](https://dev.fitbit.com/apps) dashboard to the value shown on the page
+6. Fill in the client id, secret and the scopes
+6. Press start
+
+## Credits
+
+Based on [fitbit-web-api-demo](https://github.com/jeremiahlee/fitbit-web-api-demo) from Jeremiah Lee.
---
+++ boot.js
@@ -0,0 +1,71 @@
+// If user hasn't authed with Fitbit, redirect to Fitbit OAuth Implicit Grant Flow
+var fitbitAccessToken;
+document.querySelector("#redUrl").value = window.location.href;
+if (!window.location.hash) {
+    document.querySelector("#output").style.display = `none`;
+    document.querySelector("#start").addEventListener("click", start);
+}else{
+    document.querySelector("#input").style.display = `none`;
+    var fragmentQueryParameters = {};
+    window.location.hash.slice(1).replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function($0, $1, $2, $3) { fragmentQueryParameters[$1] = $3; }
+    );
+    console.log(fragmentQueryParameters);
+    fitbitAccessToken = fragmentQueryParameters.access_token;
+    document.querySelector("#access_token").value = fitbitAccessToken;
+    document.querySelector("#data").innerText =
+      JSON.stringify(fragmentQueryParameters, null, 2) +
+      "\n\nExample Query\n" +
+      `fetch("https://api.fitbit.com/1/user/-/activities/list.json?afterDate=2022-04-01&offset=0&limit=100&sort=asc", {
+        headers: new Headers({
+            Authorization: "Bearer " + fitbitAccessToken,
+        }),
+        mode: "cors",
+        method: "GET",
+    })`;
+}
+
+
+function start(evt) {
+    evt.preventDefault();
+
+    const clientId = document.querySelector("#clientId").value;
+    const clientSecret = document.querySelector("#clientSecret").value;
+    
+    
+    const publicUrl = encodeURIComponent(window.location.href);
+    const scope = document.querySelector("#scopes").value.split(",").map(s => s.trim()).join(" ");
+    
+    
+    const authUrl =
+      "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret +
+      "&redirect_uri=" +
+      publicUrl +
+      "&scope=" + encodeURIComponent(scope);
+    console.log(authUrl);
+    window.location.replace(authUrl);
+}
+
+// Make an API request and graph it
+var processResponse = async function(res) {
+    if (!res.ok) {
+        console.log("Error: ", res);
+        await res.text().then(console.log);
+        throw new Error('Fitbit API request failed: '+ res);
+    }
+    
+    console.log(await res.json())
+    var contentType = res.headers.get('content-type')
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        return await res.json();
+    } else {
+        throw new Error('JSON expected but received ' + contentType);
+    }
+}
+
+
+  
